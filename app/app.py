e2a84dc482a0f1336fa3f35111a430c3e3744fa9
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-04-01 16:22:00 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,23 +5,22 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = "postgresql://ma_db_flask_user:xyRR3jo1vh3sPDa17xxJ9QN2M1u3UnKK@dpg-d76h6olm5p6s73bmopn0-a/ma_db_flask"
+
+@app.route("/")
+def home():
+    return {"message": "Serveur V12 avec Database connecté !"}
 
 @app.route("/db")
-def get_db_users():
+def get_users():
     try:
-        # Connexion à PostgreSQL
         conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor()
-        
-        # Requête pour lire la table que tu as créée
-        cur.execute("SELECT * FROM utilisateurs;")
-        users = cur.fetchall()
-        
+        cur.execute("SELECT * FROM utilisateurs;") # Ta table vue dans Adminer
+        rows = cur.fetchall()
         cur.close()
         conn.close()
-        
-        return jsonify({"status": "success", "data": users})
+        return jsonify({"status": "success", "utilisateurs": rows})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
         
